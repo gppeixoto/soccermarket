a4daa85a3d659551735adcda7c0f644b67cedd87
vinimoraesrc <vmrc@cin.ufpe.br>
2016-05-02 11:14:46 -0300

    modifies search from GET to POST and set cookies

--- soccermarket/core/views.py
+++ soccermarket/core/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'soccermarket.html')
+
+# def search(request):
+#     if request.method == 'POST':
+#         text = request.POST['search']
+#         users = Usuario.objects.filter(username__contains=text)
+#         questoes = Questao.objects.filter(nome__contains=text)
+#         for i in questoes:
+#             i.nome = i.nome.strip()
+#         return render(request, 'search/search.html', { 'users' : users , 'questoes' : questoes[:100] })
+#     return redirect(reverse('index'))
