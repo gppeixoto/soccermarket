a4e1d753d01a3d2ec45834aca5fac2b82b3bc727
rafaelfccg <rafaelfccg@gmail.com>
2016-05-02 09:57:49 -0300

    adding team

--- soccermarket/core/models.py
+++ soccermarket/core/models.py
@@ -3,3 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    nameCompleto = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    shield = models.ImageField(upload_to = "assets/shield")
+    imagemCamisa = models.ImageField(upload_to = "assets/shirts")
+
