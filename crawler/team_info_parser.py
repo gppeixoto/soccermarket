f6a788a2f33b2ee7fcbaae9c07a4588b05b54de2
rafaelfccg <rafaelfccg@gmail.com>
2016-05-27 20:17:30 -0300

    geting coach url

--- crawler/team_info_parser.py
+++ crawler/team_info_parser.py
@@ -11,8 +11,10 @@ class Team:
         self.name = ''
         self.country = ''
         self.badge = ''
+        self.coach_url = ''
         self.kit_image = ''
         self.full_name = ''
+        self.playersUrls = []
         
     def parse_full_name(self):
         self.full_name = self.tree.xpath('//div[@class="spielername-profil"]')[0]\
@@ -25,13 +27,19 @@ class Team:
                                 .get("src")
 
     def parse_country(self):
-        self.badge = self.tree.xpath('//td[@itemprop="addressLocality"]')[0]\
+        self.country = self.tree.xpath('//td[@itemprop="addressLocality"]')[0]\
                                 .text
+
+    def parse_coachUrl(self):
+        self.coach_url = self.tree.xpath('//div[@class="container-hauptinfo"]')[0]\
+                                .get("href")
 
     def parse_info(self):
         self.parse_full_name()
         self.parse_badge()
         self.parse_country()
+        self.parse_coachUrl()
+
 
     def to_csv(self):
         csv = ",".join([\
